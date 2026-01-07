04ebcfbe05add3d9cc16a53c4c0db304dd8103aa
sourabhaupase <88030891+sourabhaupase@users.noreply.github.com>
2026-01-07 12:18:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+// console.log(2>= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// the above null and undefined will put in a lot of confusion try to avoid using these type of comparesions
+
+
+//  === strict check means it checks value as well as datatypes
+
+console.log("2" === 2)
