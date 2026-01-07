1ec37caff2fdf3843cf48495a7f11ce9f1196727
sourabhaupase <88030891+sourabhaupase@users.noreply.github.com>
2026-01-07 07:41:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all the JS code as newer version
+ //alert (3 + 3) // we are using node not browser
+
+ console.log(3 
+    + 3)  // code readability should be high
+
+ console.log("HC")
+
+ let name = "hc"  // string
+ let age = 20 //variable
+ let isLoggedIn = false // boolean
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone
+// undefined => not defined the value
+// symbole => unique
+
+// object 
+
+// undefined is an datatype and null is an object
+
+console.log(typeof undefined); // output :undefined
+console.log(typeof null); / object
+ 
