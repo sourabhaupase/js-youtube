5850c4d4248c1f719e3d37517dc27009459b287a
sourabhaupase <88030891+sourabhaupase@users.noreply.github.com>
2026-01-07 11:35:15 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,6 +22,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+
+//========================operations==================//
+
+let value = 3;
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log( 1 + "2")
+// console.log( "1" + 2 + 2 )
+// console.log( 1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3));
+
+// console.log(true)
+
+let gameCounter = 100
+ gameCounter++;
+console.log(gameCounter);
+
+// link to study
+//  https://tc39.es/ecma262/multipage/abstarct-operations.html#sec-type-conversion
