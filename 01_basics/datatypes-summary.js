ca5a8fefa06c5a9978ec7b7619d5dfc4858cc175
sourabhaupase <88030891+sourabhaupase@users.noreply.github.com>
2026-01-09 06:04:04 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const heros = ["Superman", "Batman", "Ironman"];
+
+let myObj = {
+    name: "Hitesh",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId)
+
+// Non primitive (Reference)
+
+// Array, Objects, Functions
+
+// JS is dynamically typed language or statically typed language  ?
+
+// return types of non primitive datatype are functions and the return type of functions are called object functions
+
+
