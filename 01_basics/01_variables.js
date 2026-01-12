d6da9c91114cc27cc78d2cb5e1508a73c5ebae99
sourabhaupase <88030891+sourabhaupase@users.noreply.github.com>
2026-01-12 11:29:40 +0000

    Numbers and Maths

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = "285130"
+let accountEmail = "sourabha@google.com"
+var accountPassword = "284603"
+accountCity = "Pune"
+
+console.log(accountId)
+
+accountEmail = "hc@fc.com"
+
+console.log(accountEmail);
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+//**********Numbers*******************/
+
+const score = 500
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(typeof balance);
+
+const otherNumber = 323.1322
+
+
+// console.log(otherNumber.toPrecision(5))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/********************Maths*******************/
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(4.6))
+// console.log(Math.min(2, 6, 7, 8))
+// console.log(Math.max(3, 8, 4, 1))
+
+
+console.log(Math.ceil(Math.random()));
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
