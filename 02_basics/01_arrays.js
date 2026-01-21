f3c2398503549f8e4db98ff2973423f2f5d2e54b
sourabhaupase <88030891+sourabhaupase@users.noreply.github.com>
2026-01-21 07:08:07 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+// ==== Arrays ==== //
+const myArr = [0, 1, 2, 3, 4, 5]
+const myArray = [0, 1, 2, 3, 4]
+const myHeros = ["Superman, Batman"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[4]);
+
+// =========== Array methods ============== //
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(4));
+
+const newArr = myArr.join()
+   
+// console.log(myArr);
+// console.log(newArr);
+
+
+//// ================== slice and splice ================= ////
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+
+// slice wont manuplate the original array
+// splice manuplate the original array 
+//  this is the big difference between slice and splice
